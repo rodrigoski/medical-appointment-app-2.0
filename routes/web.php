00f5399a98e119b97022fc7f1d6b357c5935c97f
rodrigoski <carlos.rojas@tecdesoftware.edu.mx>
2026-01-28 10:08:08 -0600

    chore: admin route added

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::redirect('/','/admin');
+// Route::get('/', function () {
+//    return view('welcome');
+// });
+
+Route::get('/dashboard')->name('user.dashboard');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
