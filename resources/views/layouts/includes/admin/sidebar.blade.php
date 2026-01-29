3a0474695336b65e2346b3c2fcd847da70cd9145
rodrigoski <carlos.rojas@tecdesoftware.edu.mx>
2026-01-28 22:49:31 -0600

    feat(sidebar): add multi-level menu example + new icon link

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,47 +1,66 @@
 @php
-// Arreglo de iconos
-$links = [
-    [
-        'name' => 'Dashboard',
-        'icon' => 'fa-solid fa-gauge',
-        'href' => route('admin.dashboard'),
-        'active' => request()->routeIs('admin.dashboard'),
-    ],
-    [
-        'name' => 'Dashboard',
-        'icon' => 'fa-solid fa-gauge',
-        'href' => route('admin.dashboard'),
-        'active' => request()->routeIs('admin.dashboard'),
-    ],
-];
+    //Arreglo de íconos
+    $links = [
+        [
+            'name' => 'Dashboard',
+            'icon' => 'fa-solid fa-gauge',
+            'href' => route('admin.dashboard'),
+            'active' => request()->routeIs('admin.dashboard'),
+        ],
+        [
+            'header' => 'Hospital',
+        ],
+        [
+            'name' => 'Dashboard',
+            'icon' => 'fa-solid fa-gauge',
+            'href' => route('admin.dashboard'),
+            'active' => false,
+        ],
+    ];
 @endphp
 
-
-<aside id="top-bar-sidebar" class="fixed top-0 left-0 z-40 w-64 h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
-      <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
-         <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="Flowbite Logo" />
-         <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Flowbite</span>
-      </a>
-
-      <ul class="space-y-2 font-medium">
-      @foreach($links as $link)
-         <li>
-            <a href="{{ $link['href'] }}"
-               class="flex items-center px-2 py-1.5 rounded-base text-body
-                      hover:bg-neutral-tertiary hover:text-fg-brand group">
-
-                <!-- Icono -->
-                <span class="flex items-center justify-center w-6 h-6 text-gray-500 group-hover:text-fg-brand
-                    {{ $link['active'] ? 'bg-gray-100' : '' }}">
-                    <i class="{{ $link['icon'] }}"></i>
-                </span>
-
-                <!-- Texto -->
-                <span class="ms-3">{{ $link['name'] }}</span>
-            </a>
-         </li>
-      @endforeach
-      </ul>
-   </div>
+<aside id="logo-sidebar"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
+            @foreach ($links as $link)
+                <li>
+                    {{-- Revisa si existe definido una llave llamada 'header' --}}
+                    @isset($link['header'])
+                        <div class="px-2 py-2 text-xs font-semibold text-gray-500 uppercase">
+                            {{ $link['header'] }}
+                        </div>
+                        {{-- Si no existe, usa la etiqueta como estaba definida antes --}}
+                    @else
+                        <a href="{{ $link['href'] }}"
+                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
+                            <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
+                                <i class="{{ $link['icon'] }}"></i>
+                            </span>
+                            <span class="ms-3">{{ $link['name'] }}</span>
+                        </a>
+                    @endisset
+                </li>
+                <li>
+                <button type="button" class="flex items-center w-full justify-between px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/></svg>
+                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/></svg>
+                </button>
+                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                    <li>
+                        <a href="#" class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Products</a>
+                    </li>
+                    <li>
+                        <a href="#" class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Billing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Invoice</a>
+                    </li>
+                </ul>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </aside>
